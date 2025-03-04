37b65f072d0d913912d5d27a87b379567a6d37fa
Vildan_Gimatov <vildangimatov192@gmail.com>
2025-03-04 20:22:31 +0500

    Creat EnemyManager + enemy prefab + attacking enemy after click

---
+++ Assets/Scripts/EnemiesConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemiesConfig", menuName = "Configs/EnemiesConfig")]
+public class EnemiesConfig : ScriptableObject
+{
+    public List<EnemyData> Enemies;
+    
+}
---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class EnemyData
+{
+    public float Health;
+    public Sprite Sprite;
+}
