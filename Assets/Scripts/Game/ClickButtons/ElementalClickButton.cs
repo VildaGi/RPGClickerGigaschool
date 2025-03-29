ae35f3fcc34fc31aa3363eaef171c6e1f87e421c
Vildan_Gimatov <vildangimatov192@gmail.com>
2025-03-29 22:37:42 +0500

    Creat Elemental Buttons + Fix Next and Restart Buttons

---
+++ Assets/Scripts/Game/ClickButtons/ElementalClickButton.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.ClickButtons
+{
+    public class ElementalClickButton
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Game/ElementType.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+    public enum Element
+    {
+        Fire = 1,
+        Earth = 2,
+        Wind = 3,
+        Water = 4
+    }
+}
